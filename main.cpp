16518a5c312fa7ba8fc998a1e6877b9d1f7ae995
Red Su <yredsu@gmail.com>
2026-02-28 11:16:45 -0800

    Implement loading data onto array

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main() {
+    ifstream file("SanityCheck_DataSet__1.txt");
+    string s;
 
-    return 0;
+    int classes;
+    double values;
+
+    vector<vector<double>> class1;
+    vector<vector<double>> class2;
+
+    while (getline(file, s)) {
+        stringstream ss(s);
+        ss >> classes;
+
+        vector<double> features;
+        while (ss >> values) {
+            features.push_back(values);
+        }
+
+        if (classes == 1) 
+            class1.push_back(features);
+        else    
+            class2.push_back(features);
+    }
 }
